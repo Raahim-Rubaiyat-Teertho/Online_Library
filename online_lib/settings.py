90ca39d7df33f53fe9e1c7889487e2fa7a385e6a
Raahim-Rubaiyat-Teertho <raahim.rubaiyat2021@gmail.com>
2023-08-09 04:08:04 +0600

    connecting database

--- online_lib/settings.py
+++ online_lib/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'library',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'online_lib.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'library',
+        'USER' : 'root',
+        'HOST' : 'localhost',
+        'PORT' : 3306,
+        'PASSWORD' : '',
     }
 }
 
