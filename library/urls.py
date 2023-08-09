7d3e138bc9e60ffcf8ea35ff632920e9a2ce5914
Raahim-Rubaiyat-Teertho <raahim.rubaiyat2021@gmail.com>
2023-08-10 03:08:33 +0600

    user registration backend

--- library/urls.py
+++ library/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("register/", views.register_user, name="register_user")
+    path("register/", views.register_user, name="register_user"),
+    path("dashboard/", views.user_dashboard, name ="user_dashboard"),
 ]
--- library/views.py
+++ library/views.py
@@ -3,9 +3,29 @@ from .models import User
 # Create your views here.
 from django.http import HttpResponse
 from .models import User
+from django.db import connection
 
 def index(request):
     return render(request, 'start_page/index.html')
 
 def register_user(request):
-    return render(request, "user_reg/user_reg.html")
+    try:
+        if(request.method == "GET"):
+            fname = request.GET['firstname']
+            lname = request.GET['lastname']
+            email = request.GET['email']
+            password = request.GET['password']
+            nid = request.GET['nid']
+            phone = request.GET['phone_number']
+            address = request.GET['address']
+            age = request.GET['age']
+
+        with connection.cursor() as cursor:
+            cursor.execute("insert into user (fname, lname, email, password, nid, phone, address, age) values (%s, %s, %s, %s, %s, %s, %s, %s);", [fname, lname, email, password, nid, phone, address, age])
+
+        return render(request, 'user_reg/dashboard.html')
+    except:
+        return render(request, "user_reg/user_reg.html")
+
+def user_dashboard(request):
+    return render(request, "user_reg/dashboard.html")
