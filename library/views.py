aba9fb330b3a552aca06234e05d514b3f1528920
Saif <gluttonyenvyfmab@gmail.com>
2023-08-10 02:12:15 +0600

    user reg template

--- library/views.py
+++ library/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
-
+from .models import User
 
 def index(request):
     
     return HttpResponse("Hello, world.")
+
+def register_user(request):
+    return render(request, "user_reg/user_reg.html")
